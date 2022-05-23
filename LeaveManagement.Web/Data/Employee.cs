4a98412e2518ca68837cb8ff0673fd5dbcb7be42
Ferwhatever <fernandomldev@gmail.com>
2022-05-22 21:38:40 -0700

    Added Tables and Migrations to DB

---
+++ LeaveManagement.Web/Data/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.Web.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastNames { get; set; }
+        public string? EmployeeId { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+
+    }
+}
