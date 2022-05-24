59f4cbfd8266598aa8f8570f371942c13e7a32cf
Ferwhatever <fernandomldev@gmail.com>
2022-05-23 18:20:26 -0700

    Modificado las operaciones crud , agregado BD y swal, terminado "Tipo de permiso" Modified crud operations, added DB and swal and finished "Leave type"

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [Display(Name = "Tipo de permiso")]
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string Name { get; set; }
+        [Display(Name="Numero de dias")]
+        [Range(1,25,ErrorMessage ="Ingresa un numero Valido [1/25 dias]")]
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public int DefaultDays { get; set; }
+    }
+}
